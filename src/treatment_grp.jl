b8a476c2fd28f864a5cb698f8853f5fae03fd49d
PasoStudio73 <paso.studio73@gmail.com>
2026-03-09 21:54:21 +0100

    dataset metadata upgraded

--- src/treatment_grp.jl
+++ src/treatment_grp.jl
@@ -42,11 +42,11 @@ function discrete_encode(X::Matrix)
 end
 
 # ---------------------------------------------------------------------------- #
-#                                DataFeature                                   #
+#                              metadata structs                                #
 # ---------------------------------------------------------------------------- #
 struct DiscreteFeat{T} <: AbstractDataFeature
     id::Int
-    vname::Symbol
+    vname::String
     levels::CategoricalArrays.CategoricalVector
     valididxs::Vector{Int}
     missingidxs::Vector{Int}
@@ -58,13 +58,13 @@ struct DiscreteFeat{T} <: AbstractDataFeature
         valididxs::Vector{Int},
         missingidxs::Vector{Int}
     ) where T
-        new{T}(id, Symbol(vname), levels, valididxs, missingidxs)
+        new{T}(id, vname, levels, valididxs, missingidxs)
     end
 end
 
 struct ContinuousFeat{T} <: AbstractDataFeature
     id::Int
-    vname::Symbol
+    vname::String
     valididxs::Vector{Int}
     missingidxs::Vector{Int}
     nanidxs::Vector{Int}
@@ -76,20 +76,34 @@ struct ContinuousFeat{T} <: AbstractDataFeature
         missingidxs::Vector{Int},
         nanidxs::Vector{Int}
     ) where T
-        new{T}(id, Symbol(vname), valididxs, missingidxs, nanidxs)
+        new{T}(id, vname, valididxs, missingidxs, nanidxs)
     end
 end
 
 struct AggregateFeat{T} <: AbstractDataFeature
     id::Int
-    vname::Symbol
+    vname::String
     feat::Base.Callable
     nwin::Int
     valididxs::Vector{Int}
     missingidxs::Vector{Int}
     nanidxs::Vector{Int}
-    # hasmissing::Vector{Bool}}
-    # hasnans::Vector{Bool}}
+    hasmissing::Vector{Bool}
+    hasnans::Vector{Bool}
+
+    function AggregateFeat{T}(
+        id::Int,
+        vname::Union{String,Symbol},
+        feat::Base.Callable,
+        nwin::Int,
+        valididxs::Vector{Int},
+        missingidxs::Vector{Int},
+        nanidxs::Vector{Int},
+        hasmissing::Vector{Bool},
+        hasnans::Vector{Bool}
+    ) where T
+        new{T}(id, vname, feat, nwin, valididxs, missingidxs, nanidxs, hasmissing, hasnans)
+    end
 end
 
 struct ReduceFeat{T} <: AbstractDataFeature
@@ -99,9 +113,122 @@ struct ReduceFeat{T} <: AbstractDataFeature
     valididxs::Vector{Int}
     missingidxs::Vector{Int}
     nanidxs::Vector{Int}
-    # hasmissing::Vector{Bool}}
-    # hasnans::Vector{Bool}}
+    hasmissing::Vector{Bool}
+    hasnans::Vector{Bool}
+
+    function ReduceFeat{T}(
+        id::Int,
+        vname::Union{String,Symbol},
+        reducefunc::Base.Callable,
+        valididxs::Vector{Int},
+        missingidxs::Vector{Int},
+        nanidxs::Vector{Int},
+        hasmissing::Vector{Bool},
+        hasnans::Vector{Bool}
+    ) where T
+        new{T}(id, vname, reducefunc, valididxs, missingidxs, nanidxs, hasmissing, hasnans)
+    end
 end
+
+# ---------------------------------------------------------------------------- #
+#                               dataset structs                                #
+# ---------------------------------------------------------------------------- #
+struct DiscreteDataset
+
+end
+
+struct ContinuousDataset
+
+end
+
+struct AggregatedMultidimDataset
+
+end
+
+struct ReducedMultidimDataset
+
+end
+
+# ---------------------------------------------------------------------------- #
+#                               getter methods                                 #
+# ---------------------------------------------------------------------------- #
+"""
+    get_id(f::AbstractDataFeature) -> Int
+
+Returns the column id of the feature.
+"""
+get_id(f::AbstractDataFeature) = f.id
+
+"""
+    get_vname(f::AbstractDataFeature) -> String
+
+Returns the variable name of the feature.
+"""
+get_vname(f::AbstractDataFeature) = f.vname
+
+"""
+    get_valididxs(f::AbstractDataFeature) -> Vector{Int}
+
+Returns the indices of valid values for the feature.
+"""
+get_valididxs(f::AbstractDataFeature) = f.valididxs
+
+"""
+    get_missingidxs(f::AbstractDataFeature) -> Vector{Int}
+
+Returns the indices of `missing` values for the feature.
+"""
+get_missingidxs(f::AbstractDataFeature) = f.missingidxs
+
+"""
+    get_nanidxs(f::Union{ContinuousFeat,AggregateFeat,ReduceFeat}) -> Vector{Int}
+
+Returns the indices of `NaN` values for the feature.
+"""
+get_nanidxs(f::Union{ContinuousFeat,AggregateFeat,ReduceFeat}) = f.nanidxs
+
+"""
+    get_hasmissing(f::Union{AggregateFeat,ReduceFeat}) -> Vector{Bool}
+
+Returns the per-element flags indicating presence of `missing` values internally.
+"""
+get_hasmissing(f::Union{AggregateFeat,ReduceFeat}) = f.hasmissing
+
+"""
+    get_hasnans(f::Union{AggregateFeat,ReduceFeat}) -> Vector{Bool}
+
+Returns the per-element flags indicating presence of `NaN` values internally.
+"""
+get_hasnans(f::Union{AggregateFeat,ReduceFeat}) = f.hasnans
+
+"""
+    get_levels(f::DiscreteFeat) -> CategoricalArrays.CategoricalVector
+
+Returns the categorical levels of the discrete feature.
+"""
+get_levels(f::DiscreteFeat) = f.levels
+
+"""
+    get_feat(f::AggregateFeat) -> Base.Callable
+
+Returns the aggregation function of the feature.
+"""
+get_feat(f::AggregateFeat) = f.feat
+
+"""
+    get_nwin(f::AggregateFeat) -> Int
+
+Returns the number of windows of the aggregate feature.
+"""
+get_nwin(f::AggregateFeat) = f.nwin
+
+"""
+    get_reducefunc(f::ReduceFeat) -> Base.Callable
+
+Returns the reduce function of the feature.
+"""
+get_reducefunc(f::ReduceFeat) = f.reducefunc
+
 
 # ---------------------------------------------------------------------------- #
 #                             DataTreatment struct                             #
