e82f94bbfc89dba79a8fb9013db15a570fe084e5
PasoStudio73 <paso.studio73@gmail.com>
2026-06-12 22:29:18 +0200

    featureset checked

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ makedocs(;
         size_threshold=4000000,
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://PasoStudio73.github.io/DataTreatments.jl",
-        edit_link="main", # possibly this line is dangerous after publishing
+        edit_link="dev", # possibly this line is dangerous after publishing
         assets=String[],
     ),
     pages=[
